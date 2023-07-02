f606f06d773401e516f6fd72a2f47130e57603b6
Bhuvan Agarwal <bhuvanagarwal243@gmail.com>
2023-07-02 12:26:39 +0530

    First Commit

---
+++ lib/widgets/home_widgets/custom_appBar.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:women_safety_app/utils/quotes.dart';
+
+class CustomAppBar extends StatelessWidget {
+  // const CustomAppBar({super.key});
+  Function? onTap;
+  int? quoteIndex;
+  CustomAppBar({this.onTap, this.quoteIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap!();
+      },
+      child: Container(
+        child: Text(
+          sweetSayings[quoteIndex!],
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
